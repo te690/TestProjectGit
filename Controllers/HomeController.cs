d6383b1bc4bdbcf70be52534b99a859b8e65f9ff
te690 <satya.roy30@outlook.com>
2023-04-10 15:25:11 +0530

    hi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,9 @@ namespace TestProjectGit.Controllers
         {
             return View();
         }
+            
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
